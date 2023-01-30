5c43535ed954ee65ec46736c2bda23feb92fb33b
Florina <ffiador@gmail.com>
2023-01-30 13:55:50 +0000

    user routes

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,16 +4,16 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   first_name: 
     {type: String,
-    required: true},
+    },
   last_name: 
     {type: String,
-    required: true},
+    },
   email: 
     {type: String,
-    required: true},
+    },
   password: 
     {type: String,
-    required: true},
+    },
 });
 
 const UserModel = mongoose.model("users", userSchema);
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -2,20 +2,64 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const asyncHandler = require("express-async-handler");
 
 
-// const userRoutes = (app) => {
-//    //this is for registering:
-//   app.post(`/api/register`,async (req,res) =>{
-//     const {firstname, lastname, email, password} = req.body
-//     if (!firstname ||!lastname ||!email ||!password){
-//       res.status(400)
-//       throw new Error("Please add all fields")
-//     }
-//     res.json({message: "Register User"})
-//   })
+const userRoutes = (app) => {
+//    //this is for registering new user info to mongodb: 
+  app.post(`/api/register`,async (req,res) =>{
+    const {first_name, last_name, email, password} = req.body
 
+//- first I throw an error if all fields are not filled
+    if (!first_name || !last_name || !email || !password){
+      res.status(400)
+      throw new Error('Please add all fields')
+    }
+
+//check if user exists by email
+    const userExists = await User.findOne({email})
+    if (userExists){
+      res.status(400)
+      throw new Error("User already exists"),
+      console.log(req.body, "User already exists")
+    }
+//hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+//creating a user - this adds the req user info to our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      password: hashedPassword,
+      email})
+    
+    if(user) {
+      res.status(202).json({
+        _id: user.id,
+        first_name: user.first_name,
+        email: user.email,
+      })
+    }else {
+      res.status(400)
+      throw new Error("Invalid user data")
+    }
+    console.log(req.body)
+  })
+//auth new user
+
+// const registerUser = await User.findOne({ email })
+
+//   if (user && (await bcrypt.compare(password, user.password))) {
+//     res.json({
+//       _id: user.id,
+//       name: user.name,
+//       email: user.email,
+//       token: generateToken(user._id),
+//     })
+//   } else {
+//     res.status(400)
+//     throw new Error('Invalid credentials')
+//   }
 
   //This is for signing in:
   app.post(`/api/signin`, async (req, res) => {
@@ -71,6 +115,6 @@ const asyncHandler = require("express-async-handler");
   //     user,
   //   });
   // });
-// };
+};
 
 module.exports = userRoutes;
