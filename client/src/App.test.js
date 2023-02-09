3a144b401b265fe3835f3b10100c1bbe7001121e
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-02-09 20:35:58 +0000

    Removed duplicate App component from test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,29 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { render, screen } from '@testing-library/react';
-
-import Homepage from "./pages/Homepage"
-import About from "./pages/About"
-import EditProfile from "./components/EditProfile"
-import SignIn from "./components/SignIn";
-import Register from "./components/Register";
-import UserHome from "./pages/UserHome";
-import ProfilePage from "./pages/ProfilePage";
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Route exact path="/" component={Homepage} />
-      <Route path="/about" component={About} />
-      <Route path="/editprofile" component={EditProfile} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/register" component={Register} />
-      <Route path="/userhome" component={UserHome} />
-      <Route path="/profilepage/:id" component={ProfilePage} />
-    </BrowserRouter>
-  );
-}
+import App from './App';
 
 test('renders the landing page', () => {
     render(<App />);
