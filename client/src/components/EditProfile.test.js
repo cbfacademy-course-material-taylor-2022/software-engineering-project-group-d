bab6e74b0a7ccf7b13002e4f07c1e6e5f6c84d3a
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-02-09 19:25:42 +0000

    Removed unnecessary React import, reverted back to 'test' instead of 'it'

--- client/src/components/EditProfile.test.js
+++ client/src/components/EditProfile.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import EditProfile from './EditProfile';
 
-it('renders EditProfile component', () => {
+test('renders EditProfile component', () => {
   render(<EditProfile />);
   const testElement = screen.getByText(/Welcome to Passion4Travel website/i)
   expect(testElement).toBeInTheDocument();
