e91d660a99215880a210c911a6e2c3bcdb573ecb
Florina <ffiador@gmail.com>
2023-02-11 08:45:27 +0000

    added the recommended changes for the tests

--- client/src/tests/Fetchdata.test.js
+++ client/src/tests/Fetchdata.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Fetchdata from '../components/Fetchdata';
 
 test('renders Fetchdata component', () => {
